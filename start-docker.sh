22465a2947b905d26c8c5f196e512f4461ac13a9
csweichel <christian.weichel@typefox.io>
2020-10-07 07:29:41 +0000

    better startup script

--- start-docker.sh
+++ start-docker.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+export XDG_RUNTIME_DIR=/home/gitpod/.docker/run
+export DOCKER_HOST=unix:///home/gitpod/.docker/run/docker.sock
+
 dockerd-rootless.sh --experimental --iptables=false --data-root /workspace/docker
